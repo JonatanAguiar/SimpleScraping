713c5db60dd772a6f3dc79da20c0fac3230624f6
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2021-04-17 21:51:36 -0300

    Scraping criando arquivo JSON de mais filmes

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-const URl = 'https://www.adorocinema.com/filmes/filme-221542/';
-//const URl = 'https://www.imdb.com/title/tt0102926/?ref_=nv_1';
+// const URL = 'https://www.adorocinema.com/filmes/filme-221542/';
+const URLS = [
+    'https://www.imdb.com/title/tt0102926/', 
+    'https://www.imdb.com/title/tt2267998/'
+];
 
 (async () => {
-    const response = await request(URl);
-    let $ = cheerio.load(response);
+    let moviesData = [];
+    for(let movie of URLS){
+        const response = await request({
+            uri: movie,
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
+            },
+            gzip: true,
+        });
+        let $ = cheerio.load(response);
 
-    // let title = $('div[class="title_wrapper"] > h1').text();
-    let title = $('div[class="section-wrap breadcrumb "] > h1[class="item"]').text();
-    let releaseDate = $('span[class="ACrL2ZACrpbG1lcy9hZ2VuZGEvd2Vlay0yMDIxLTA3LTIyLw== date blue-link"]').text();
+        let title = $('div[class="title_wrapper"] > h1').text().trim();
+        let rating = $('div[class="ratingValue"] > strong > span').text();
+        let poster = $('div[class="poster"] > a > img').attr('src');
+        let totalRatings = $('div[class="imdbRating"] > a').text();
+        let releaseDate = $('a[title="See more release dates"]').text().trim();
 
-    console.log(title.trim());
-    console.log(releaseDate.trim());
+        let genres = [];
+        $('div[class="title_wrapper"] a[href^="/search/title?genres="]').each((i, elm) => {
+            let genre = $(elm).text();
+            genres.push(genre);
+        });
+
+        moviesData.push({
+            title,
+            rating,
+            poster,
+            totalRatings,
+            releaseDate,
+            genres
+        })
+
+        // console.log(`title: ${title}`);
+        // console.log(`rating: ${rating}`);
+        // console.log(`poster: ${poster}`);
+        // console.log(`totalRatings: ${totalRatings}`);
+        // console.log(`releaseDate: ${releaseDate}`);
+        // console.log(`genres: ${genres}`);
+    }
+    
+    fs.writeFileSync('./data.json', JSON.stringify(moviesData), 'utf-8');
+
+    console.log(`moviesData: ${moviesData}`);
+    
 })()
